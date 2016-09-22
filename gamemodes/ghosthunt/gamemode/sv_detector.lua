df796e2fa23907aaba7c3b62fbb9f35a61daa720
Dan <DJHook98@users.noreply.github.com>
2016-09-22 12:50:17 +0100

    Updated detector script to new system

--- gamemodes/ghosthunt/gamemode/sv_detector.lua
+++ gamemodes/ghosthunt/gamemode/sv_detector.lua
@@ -6,20 +6,20 @@
     
 ---------------------------------------------------------------------------*/
 
--- Make a variable that will store the ghost detector entity 
-GHOST_DETECTOR_ENT = nil 
-
 -- Only runs if the map is supported 
 if ( MAP_SUPPORTED ) then 
+	-- Make a variable that will store the ghost detector entity 
+	GAMEMODE.GHOST_DETECTOR_ENT = nil 
+	
 	--
 	-- This hook tries to find the ghost detector entity in the map 
 	--
 	hook.Add( "InitPostEntity", "DetectorFind", function()					
 		-- Checks every entity in the map on startup to try and find the ghost detector entity
 		for _, ent in ipairs( ents.GetAll() ) do		
-			if ( table.HasValue( GHOST_DETECTORS, ent:GetName() ) ) then 
+			if ( ent:GetName() == self.GhostDetector ) then 
 				print( "[GHOSTHUNT] Ghost detector entity found in map!\n" )
-				GHOST_DETECTOR_ENT = ent
+				GAMEMODE.GHOST_DETECTOR_ENT = ent
 			end 
 		end 
 	end )
